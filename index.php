a7186261d2c52a856716442d6257cbc395d5cee2
elly <eleanorz@gmail.com>
2014-05-03 19:16:43 -0700

    added in basic FB sdk JS scripting, doesn't break anything

--- index.php
+++ index.php
@@ -5,6 +5,25 @@
 </head>
 <body>
 
+<script>
+// Generic FB SDK access:
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{your-app-id}',
+      xfbml      : true,
+      version    : 'v2.0'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
 <?php 
 
 /**
